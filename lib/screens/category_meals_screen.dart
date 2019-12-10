c965dc3f89133b8cbabfe8a6b1dc3600674e6e04
Julio Martins de Sousa <juliomartinssousa@gmail.com>
2019-12-10 20:38:28 -0300

    Add category items list and Meal model

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
 
+import '../mocks/dummy-data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
+    final Map<String, String> routeArgs =
+        ModalRoute.of(context).settings.arguments;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('The recipes'),
+        title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text(
-          'The recipes for the category',
-        ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
